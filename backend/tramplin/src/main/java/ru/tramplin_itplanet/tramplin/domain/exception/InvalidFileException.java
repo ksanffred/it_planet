d6d103c23c1dd4f023b7a0b79bf0e604c62ec8a2
fyodor zakharov <ksanffred2@gmail.com>
2026-03-27 00:59:48 +0300

    s3 media storage

---
+++ backend/tramplin/src/main/java/ru/tramplin_itplanet/tramplin/domain/exception/InvalidFileException.java
@@ -0,0 +1,8 @@
+package ru.tramplin_itplanet.tramplin.domain.exception;
+
+public class InvalidFileException extends RuntimeException {
+
+    public InvalidFileException(String message) {
+        super(message);
+    }
+}
