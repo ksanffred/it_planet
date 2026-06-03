8c19725bc0f9e7249f013bb7c48dac079cb8dbbf
TraumDE <eugene301workmaik@gmail.com>
2026-06-04 01:45:57 +0300

    feat: move svg from assets to vue components and add decor to main page

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-const route = useRoute()
-const router = useRouter()
+const route = useRoute();
+const router = useRouter();
 
-const showMap = computed(() => route.query.view === 'map')
+const showMap = computed(() => route.query.view === "map");
 
 const toggleMap = (value: boolean) => {
   router.push({
     query: {
       ...route.query,
-      view: value ? 'map' : undefined,
+      view: value ? "map" : undefined,
     },
-  })
-}
+  });
+};
 </script>
 
 <template>
@@ -25,10 +25,38 @@ const toggleMap = (value: boolean) => {
         <SectionsMapSection />
       </template>
       <template v-else>
+        <SvgArcShape class="arc-shape" />
+        <SvgCircleShape class="circle-shape" />
+        <SvgBlockShape class="block-shape" />
+
         <SectionsOpportunitiesSection />
       </template>
     </main>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.arc-shape {
+  position: absolute;
+  transform: translate(200px, 750px) rotate(20deg);
+
+  width: 184px;
+  height: 92px;
+}
+
+.circle-shape {
+  position: absolute;
+  transform: translate(700px, 300px);
+
+  width: 150px;
+  height: 150px;
+}
+
+.block-shape {
+  position: absolute;
+  transform: translate(700px, 1100px);
+
+  width: 164px;
+  height: 164px;
+}
+</style>
